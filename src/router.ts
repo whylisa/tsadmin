39e55f9701b90ddd11b374c163d19e4f6d4ec498
haoyue.wang <843655240@qq.com>
2019-08-13 17:58:37 +0800

    路由修改

--- src/router.ts
+++ src/router.ts
@@ -17,9 +17,6 @@ export default new Router({
     {
       path: '/home',
       name: 'Home',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: Home,
 			children: [
 				{
